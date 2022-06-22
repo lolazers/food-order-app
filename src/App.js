90e7f4ee1fd514855e24c4deace63830604206d2
Jon Ng <jonathanng15@gmail.com>
2022-06-22 01:30:25 -0700

    Added MealOptions component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
 import Header from './Components/Header';
+import MealOptions from './Components/MealOptions';
 
-const ParentContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  /* background-color: #222222; */
-  /* height: 100vh; */
-`;
+// const ParentContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   /* background-color: #222222; */
+//   /* height: 100vh; */
+// `;
 
 function App() {
   return (
-    <ParentContainer className="App">
+    <div className="App">
       <Header />
-    </ParentContainer>
+      <MealOptions />
+    </div>
   );
 }
 
